eaa3848fc5f095cf4c457c4bf561f31c08e90d70
wozehua <273362054@qq.com>
2023-09-04 21:49:40 +0800

    feat:通用值转换

---
+++ CommonFeatrue/Utils/ParseUtils.cs
@@ -0,0 +1,30 @@
+﻿namespace CommonFeatrue.Utils
+{
+    public static class ParseUtils
+    {
+        /// <summary>
+        /// 通用值类型转换
+        /// </summary>
+        /// <typeparam name="T">转换值类型</typeparam>
+        /// <param name="value">被转换的值</param>
+        /// <param name="defaultValue">默认值</param>
+        /// <returns>转换后的值或默认值</returns>
+        public static T Parse<T>(this object value, T defaultValue = default)
+        {
+            try
+            {
+                var type = typeof(T);
+                var nullableType = Nullable.GetUnderlyingType(type);
+                if (nullableType is not null)
+                {
+                    return (T)Convert.ChangeType(value, nullableType);
+                }
+                return (T)Convert.ChangeType(value, type);
+            }
+            catch
+            {
+                return default;
+            }
+        }
+    }
+}
